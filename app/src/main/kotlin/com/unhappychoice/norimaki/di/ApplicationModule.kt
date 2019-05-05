885987876ec769b94d4ee6de27962ecba24a39a9
yuelululu <91594381+yuelululu@users.noreply.github.com>
2019-05-05 21:42:47 +0000

    Fix test command

--- app/src/main/kotlin/com/unhappychoice/norimaki/di/ApplicationModule.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/di/ApplicationModule.kt
@@ -4,4 +4,5 @@ import com.github.unhappychoice.circleci.CircleCIAPIClient
 import com.github.unhappychoice.circleci.CircleCIAPIClientV1
 import com.google.gson.FieldNamingPolicy
 import com.google.gson.Gson
-import com.google.gson.G
+import com.google.gson.GsonBuilder
+import com.unhappychoice.norimaki.N
