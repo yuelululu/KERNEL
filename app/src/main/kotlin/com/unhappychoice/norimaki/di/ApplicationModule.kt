5f9830d5fad7f14abf600a7f63149bfd229f0e87
yuelululu <91594381+yuelululu@users.noreply.github.com>
2019-05-05 11:53:36 +0000

    msm: mdss: add init and deinit calls to notify update ioctl

--- app/src/main/kotlin/com/unhappychoice/norimaki/di/ApplicationModule.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/di/ApplicationModule.kt
@@ -2,4 +2,5 @@ package com.unhappychoice.norimaki.di
 
 import com.github.unhappychoice.circleci.CircleCIAPIClient
 import com.github.unhappychoice.circleci.CircleCIAPIClientV1
-import com
+import com.google.gson.FieldNamingPolicy
+import com.
