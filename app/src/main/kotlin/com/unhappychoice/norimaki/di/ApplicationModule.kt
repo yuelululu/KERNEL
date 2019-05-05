f9cc55538f4489543f70fcbf31b773d1576419a5
yuelululu <91594381+yuelululu@users.noreply.github.com>
2019-05-05 12:19:00 +0000

    Close #9

--- app/src/main/kotlin/com/unhappychoice/norimaki/di/ApplicationModule.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/di/ApplicationModule.kt
@@ -3,4 +3,5 @@ package com.unhappychoice.norimaki.di
 import com.github.unhappychoice.circleci.CircleCIAPIClient
 import com.github.unhappychoice.circleci.CircleCIAPIClientV1
 import com.google.gson.FieldNamingPolicy
-import com.
+import com.google.gson.Gson
+import com.google.gson.G
