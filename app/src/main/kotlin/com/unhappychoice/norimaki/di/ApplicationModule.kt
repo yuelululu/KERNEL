35e123c553ceb616ada8b28991368b4df345bf85
yuelululu <91594381+yuelululu@users.noreply.github.com>
2019-05-05 08:54:35 +0000

    Updated TODOs.

--- app/src/main/kotlin/com/unhappychoice/norimaki/di/ApplicationModule.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/di/ApplicationModule.kt
@@ -1,4 +1,5 @@
 package com.unhappychoice.norimaki.di
 
 import com.github.unhappychoice.circleci.CircleCIAPIClient
-import com.github.unhappychoice.cir
+import com.github.unhappychoice.circleci.CircleCIAPIClientV1
+import com
