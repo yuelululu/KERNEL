f9b0189861ae2020a3f11f0d1032728fb1ce3133
yuelululu <91594381+yuelululu@users.noreply.github.com>
2019-05-06 18:00:08 +0000

    changed pom

--- app/src/main/kotlin/com/unhappychoice/norimaki/di/ApplicationModule.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/di/ApplicationModule.kt
@@ -5,4 +5,5 @@ import com.github.unhappychoice.circleci.CircleCIAPIClientV1
 import com.google.gson.FieldNamingPolicy
 import com.google.gson.Gson
 import com.google.gson.GsonBuilder
-import com.unhappychoice.norimaki.N
+import com.unhappychoice.norimaki.NorimakiApplication
+import com.unhappyc
