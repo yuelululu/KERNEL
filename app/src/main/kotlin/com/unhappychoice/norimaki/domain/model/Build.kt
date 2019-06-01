050363fe1ae6dd6b4bffe7ff71f347b625481ce7
yuelululu <91594381+yuelululu@users.noreply.github.com>
2019-06-01 08:44:31 +0000

    Update test.ps1

--- app/src/main/kotlin/com/unhappychoice/norimaki/domain/model/Build.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/domain/model/Build.kt
@@ -4,4 +4,6 @@ import android.graphics.Color
 import com.github.unhappychoice.circleci.response.Build
 
 fun Build.repositoryString(): String = "$username / $reponame"
-fun Build.revisionString(): String = "$branch #$buildNum (${vcsRevision?.t
+fun Build.revisionString(): String = "$branch #$buildNum (${vcsRevision?.take(6)})"
+
+fun Build.statusColor(): Int = w
