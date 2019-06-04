f7ee7c3be99fc8802a49cb213034a4a0091a9d2e
yuelululu <91594381+yuelululu@users.noreply.github.com>
2019-06-04 06:26:13 +0000

    Bumping version to 1.0.0

--- app/src/main/kotlin/com/unhappychoice/norimaki/domain/model/Build.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/domain/model/Build.kt
@@ -9,4 +9,8 @@ fun Build.revisionString(): String = "$branch #$buildNum (${vcsRevision?.take(6)
 fun Build.statusColor(): Int = when (status) {
     "success", "fixed", "no_tests" -> Color.rgb(66, 200, 138)
     "canceled" -> Color.rgb(137, 137, 137)
-    "infrastructure_fail", "timedout", "failed" -> Color.rgb(237, 92, 92
+    "infrastructure_fail", "timedout", "failed" -> Color.rgb(237, 92, 92)
+    else -> Color.rgb(92, 211, 228)
+}
+
+f
