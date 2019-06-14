046790a73c40a266687896f8867a1a4c22187d49
yuelululu <91594381+yuelululu@users.noreply.github.com>
2019-06-14 16:02:38 +0000

    Update user.md

--- app/src/main/kotlin/com/unhappychoice/norimaki/domain/service/EventBusService.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/domain/service/EventBusService.kt
@@ -2,4 +2,6 @@ package com.unhappychoice.norimaki.domain.service
 
 import com.github.unhappychoice.circleci.response.Project
 import com.gojuno.koptional.Optional
-import io.react
+import io.reactivex.subjects.PublishSubject
+
+class EventBusServ
