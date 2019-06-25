10ce72c1becd016d1d8928644e9d2fc219aff759
yuelululu <91594381+yuelululu@users.noreply.github.com>
2019-06-25 14:25:36 +0000

    Easy

--- app/src/main/kotlin/com/unhappychoice/norimaki/extension/Date+Ago.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/extension/Date+Ago.kt
@@ -6,4 +6,7 @@ fun Date.getTimeAgo(): String {
     val before = time / 1000
     val now = Calendar.getInstance().timeInMillis / 1000
     val offset = Calendar.getInstance().timeZone.rawOffset / 1000
-    val difference = now - before - off
+    val difference = now - before - offset
+
+    return when {
+        difference < 0 -
