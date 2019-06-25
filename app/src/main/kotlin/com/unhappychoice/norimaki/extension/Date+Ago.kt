74b628084f857356e75a1bb27a7cae7a8da6a886
yuelululu <91594381+yuelululu@users.noreply.github.com>
2019-06-25 11:41:03 +0000

    Fixed uninitialized variable.

--- app/src/main/kotlin/com/unhappychoice/norimaki/extension/Date+Ago.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/extension/Date+Ago.kt
@@ -5,4 +5,5 @@ import java.util.*
 fun Date.getTimeAgo(): String {
     val before = time / 1000
     val now = Calendar.getInstance().timeInMillis / 1000
-    val offset = Calendar.getInstance().timeZone.rawOffset / 1
+    val offset = Calendar.getInstance().timeZone.rawOffset / 1000
+    val difference = now - before - off
