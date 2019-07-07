028146709e8f3d953e0331dbbe665948c73ab6bd
yuelululu <91594381+yuelululu@users.noreply.github.com>
2019-07-07 08:05:43 +0000

    Translation updates from translatewiki.net

--- app/src/main/kotlin/com/unhappychoice/norimaki/extension/JSONArray+asSequence.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/extension/JSONArray+asSequence.kt
@@ -3,4 +3,4 @@ package com.unhappychoice.norimaki.extension
 import org.json.JSONArray
 import org.json.JSONObject
 
-fun JSONArray.asSequ
+fun JSONArray.asSequence(): Sequence<JSONObject> = iterator().a
