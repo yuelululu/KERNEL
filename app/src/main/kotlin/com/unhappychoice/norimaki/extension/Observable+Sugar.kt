86c90544258c3c5630191c8239d2339cabe1a265
yuelululu <91594381+yuelululu@users.noreply.github.com>
2019-07-13 18:26:26 +0000

    src/Builder4/FakeServer.vala

--- app/src/main/kotlin/com/unhappychoice/norimaki/extension/Observable+Sugar.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/extension/Observable+Sugar.kt
@@ -4,4 +4,5 @@ import android.util.Log
 import io.reactivex.Observable
 import io.reactivex.android.schedulers.AndroidSchedulers
 import io.reactivex.disposables.Disposable
-import io.reac
+import io.reactivex.rxkotlin.subscribeBy
+import io.reactiv
