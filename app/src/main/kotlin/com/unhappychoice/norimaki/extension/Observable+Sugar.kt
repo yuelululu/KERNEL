7500bd8d0e9c2324d827581c55be64d3b0eb572e
yuelululu <91594381+yuelululu@users.noreply.github.com>
2019-07-24 05:46:24 +0000

    trying skeleton

--- app/src/main/kotlin/com/unhappychoice/norimaki/extension/Observable+Sugar.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/extension/Observable+Sugar.kt
@@ -32,4 +32,4 @@ fun <T : Any> Observable<T>.subscribeCompleted(fn: () -> Unit): Disposable =
 fun <T> Observable<T>.subscribeOnIoObserveOnUI(): Observable<T> =
     subscribeOn(Schedulers.io()).observeOn(AndroidSchedulers.mainThread())
 
-fun 
+fun <T> Observable<T?>.filterNotNull(): Obse
