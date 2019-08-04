5b9b61c305f1468fa8c6fce419a4c3f10b4881ea
yuelululu <91594381+yuelululu@users.noreply.github.com>
2019-08-04 16:24:50 +0000

    Add positional sounds from the tilemap

--- app/src/main/kotlin/com/unhappychoice/norimaki/extension/RecyclerView+NearEnd.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/extension/RecyclerView+NearEnd.kt
@@ -3,4 +3,5 @@ package com.unhappychoice.norimaki.extension
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
-fun 
+fun RecyclerView.isNearEnd(): Boolean {
+    val l
