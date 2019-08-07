fac6fc650e8588218bc702a60f47c1a98f12e68d
yuelululu <91594381+yuelululu@users.noreply.github.com>
2019-08-07 05:37:05 +0000

    f2fs: avoid f2fs_lock_op in f2fs_write_begin

--- app/src/main/kotlin/com/unhappychoice/norimaki/extension/RecyclerView+NearEnd.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/extension/RecyclerView+NearEnd.kt
@@ -4,4 +4,5 @@ import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
 fun RecyclerView.isNearEnd(): Boolean {
-    val l
+    val layoutManager = layoutManager as LinearLayoutManager
+    
