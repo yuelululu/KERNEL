f5765437b95eaa2f14eac6322dff12b740e098f2
yuelululu <91594381+yuelululu@users.noreply.github.com>
2019-08-15 21:18:51 +0000

    misc bugfixes

--- app/src/main/kotlin/com/unhappychoice/norimaki/extension/ViewPresenter+Flow.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/extension/ViewPresenter+Flow.kt
@@ -2,4 +2,6 @@ package com.unhappychoice.norimaki.extension
 
 import android.content.Context
 import android.view.View
-import com.unhappychoice.norimaki.presentation.screen.core.Scre
+import com.unhappychoice.norimaki.presentation.screen.core.Screen
+import flow.Flow
+import mortar.ViewPresen
