0216ac4acafdd9d4838c7e9380cdc56887e9c371
yuelululu <91594381+yuelululu@users.noreply.github.com>
2019-08-19 22:11:28 +0000

    Some tidying

--- app/src/main/kotlin/com/unhappychoice/norimaki/extension/ViewPresenter+Flow.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/extension/ViewPresenter+Flow.kt
@@ -6,4 +6,5 @@ import com.unhappychoice.norimaki.presentation.screen.core.Screen
 import flow.Flow
 import mortar.ViewPresenter
 
-fun <V : View> ViewPresenter<V>.goTo(context: Context, screen: Screen) = Flow.get(context).se
+fun <V : View> ViewPresenter<V>.goTo(context: Context, screen: Screen) = Flow.get(context).set(screen)
+fun <V : View> ViewPr
