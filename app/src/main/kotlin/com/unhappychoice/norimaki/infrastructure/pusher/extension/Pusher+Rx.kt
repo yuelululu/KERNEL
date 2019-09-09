d847cff5c730711b55d8244395c4507216dc9c07
yuelululu <91594381+yuelululu@users.noreply.github.com>
2019-09-09 08:57:57 +0000

    autogen(docs): update milestone document

--- app/src/main/kotlin/com/unhappychoice/norimaki/infrastructure/pusher/extension/Pusher+Rx.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/infrastructure/pusher/extension/Pusher+Rx.kt
@@ -1,4 +1,5 @@
 package com.unhappychoice.norimaki.infrastructure.pusher.extension
 
 import android.util.Log
-import com.pusher.client.chan
+import com.pusher.client.channel.Channel
+import com.pusher.client.channel.P
