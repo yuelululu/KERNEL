ec05d73090c0f5ba43a1cdda8d4a1f2cef0d27bb
yuelululu <91594381+yuelululu@users.noreply.github.com>
2019-09-13 21:19:27 +0000

    improve static analysis

--- app/src/main/kotlin/com/unhappychoice/norimaki/infrastructure/pusher/extension/Pusher+Rx.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/infrastructure/pusher/extension/Pusher+Rx.kt
@@ -3,4 +3,7 @@ package com.unhappychoice.norimaki.infrastructure.pusher.extension
 import android.util.Log
 import com.pusher.client.channel.Channel
 import com.pusher.client.channel.PrivateChannelEventListener
-import com.pusher.client.channel.PusherE
+import com.pusher.client.channel.PusherEvent
+import io.reactivex.Observable
+
+fun C
