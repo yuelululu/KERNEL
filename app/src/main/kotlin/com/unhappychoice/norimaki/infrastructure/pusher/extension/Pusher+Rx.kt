0402456c34a9cfbf8f8204443acdb2e83fc13541
yuelululu <91594381+yuelululu@users.noreply.github.com>
2019-09-28 10:49:24 +0000

    #merge_ours Updated POM version to next version

--- app/src/main/kotlin/com/unhappychoice/norimaki/infrastructure/pusher/extension/Pusher+Rx.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/infrastructure/pusher/extension/Pusher+Rx.kt
@@ -15,4 +15,4 @@ fun Channel.privateChannelEvents(eventName: String): Observable<String> {
             }
 
             override fun onSubscriptionSucceeded(channelName: String?) {
-    
+                Log.d("Pusher", "Succeeded to subscr
