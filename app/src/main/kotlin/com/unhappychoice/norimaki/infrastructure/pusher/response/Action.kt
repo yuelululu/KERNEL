7ade73c9edf22758ee45ac459493246e273cc27b
yuelululu <91594381+yuelululu@users.noreply.github.com>
2019-10-11 22:01:07 +0000

    Fix prettier

--- app/src/main/kotlin/com/unhappychoice/norimaki/infrastructure/pusher/response/Action.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/infrastructure/pusher/response/Action.kt
@@ -3,4 +3,7 @@ package com.unhappychoice.norimaki.infrastructure.pusher.response
 data class Action(
     val step: Int,
     val index: Int,
- 
+    val messages: String?,
+    val log: ActionLog
+)
+
