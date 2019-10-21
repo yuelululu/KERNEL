44be236691615500433d4e5e8e444c34684c63ed
yuelululu <91594381+yuelululu@users.noreply.github.com>
2019-10-21 12:31:03 +0000

    Up.

--- app/src/main/kotlin/com/unhappychoice/norimaki/presentation/adapter/ProjectAdapter.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/presentation/adapter/ProjectAdapter.kt
@@ -6,4 +6,4 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.
+import com.github.unhappychoice.circleci.response.Pr
