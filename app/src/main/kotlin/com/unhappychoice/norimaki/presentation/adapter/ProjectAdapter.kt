303e438714a4fd7735b4fd366e5c5339f781eae3
yuelululu <91594381+yuelululu@users.noreply.github.com>
2019-10-21 08:52:07 +0000

    UTF-8 environment: be a little bit more defensive

--- app/src/main/kotlin/com/unhappychoice/norimaki/presentation/adapter/ProjectAdapter.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/presentation/adapter/ProjectAdapter.kt
@@ -3,4 +3,6 @@ package com.unhappychoice.norimaki.presentation.adapter
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
-import android.view.Vie
+import android.view.ViewGroup
+import android.widget.TextView
+import
