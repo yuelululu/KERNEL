b953af06514c5c33c5b64d324e43c508e040b9b5
yuelululu <91594381+yuelululu@users.noreply.github.com>
2019-11-06 20:38:33 +0000

    update dashboard

--- app/src/main/kotlin/com/unhappychoice/norimaki/presentation/adapter/ProjectAdapter.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/presentation/adapter/ProjectAdapter.kt
@@ -28,4 +28,5 @@ class ProjectAdapter(val context: Context) : RecyclerView.Adapter<ProjectAdapter
 
     override fun getItemCount(): Int = projects.value.size
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolde
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val view = LayoutInflater.f
