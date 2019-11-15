48a9778ad2c31c5d935dd30a7e2a30af558a3ea5
yuelululu <91594381+yuelululu@users.noreply.github.com>
2019-11-15 13:39:40 +0000

    Update training.md

--- app/src/main/kotlin/com/unhappychoice/norimaki/presentation/adapter/ProjectAdapter.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/presentation/adapter/ProjectAdapter.kt
@@ -37,4 +37,4 @@ class ProjectAdapter(val context: Context) : RecyclerView.Adapter<ProjectAdapter
         holder.bind(projects.value[position])
     }
 
-    inner class ViewHolder(va
+    inner class ViewHolder(val view: View) : RecyclerView.ViewHolder(view) 
