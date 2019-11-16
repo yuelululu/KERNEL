8c8617f3326f5f94994d2ec8bd3d6a2471c8909d
yuelululu <91594381+yuelululu@users.noreply.github.com>
2019-11-16 20:50:47 +0000

    Clean up css

--- app/src/main/kotlin/com/unhappychoice/norimaki/presentation/adapter/ProjectAdapter.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/presentation/adapter/ProjectAdapter.kt
@@ -41,4 +41,4 @@ class ProjectAdapter(val context: Context) : RecyclerView.Adapter<ProjectAdapter
         private val projectNameView = view.findViewById<TextView>(R.id.projectNameView)
 
         fun bind(project: Project) {
-            projectN
+            projectNameView.text = "${project.username}/${pr
