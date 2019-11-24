8f4eb1ad4cb53aad408ab916983882094ccf4539
yuelululu <91594381+yuelululu@users.noreply.github.com>
2019-11-24 09:38:50 +0000

    Remove debug printing.

--- app/src/main/kotlin/com/unhappychoice/norimaki/presentation/core/GsonParceler.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/presentation/core/GsonParceler.kt
@@ -3,4 +3,5 @@ package com.unhappychoice.norimaki.presentation.core
 import android.os.Parcel
 import android.os.Parcelable
 import com.google.gson.Gson
-import com.google.gson
+import com.google.gson.stream.JsonReader
+import com.google.gson.stream.Json
