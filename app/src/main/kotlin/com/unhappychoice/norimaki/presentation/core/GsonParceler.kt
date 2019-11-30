a7943901835b9328d77c624f9c105f5d262ac8e7
yuelululu <91594381+yuelululu@users.noreply.github.com>
2019-11-30 17:32:15 +0000

    Remove debugging.

--- app/src/main/kotlin/com/unhappychoice/norimaki/presentation/core/GsonParceler.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/presentation/core/GsonParceler.kt
@@ -11,4 +11,5 @@ import java.io.StringReader
 import java.io.StringWriter
 
 /**
- * From https://github.com/pyricau/dagger2-mortar-flow-experiment/blob/master/app/src/main/java/dag
+ * From https://github.com/pyricau/dagger2-mortar-flow-experiment/blob/master/app/src/main/java/dagger/demo/mortarflow/GsonParceler.java
+ */
