d55222f0a25b26571d5af138fb43ae7680507bba
yuelululu <91594381+yuelululu@users.noreply.github.com>
2020-01-15 12:21:42 +0000

    Fullfillment

---
+++ app/src/main/kotlin/com/unhappychoice/norimaki/presentation/core/ScreenChanger.kt
@@ -0,0 +1,12 @@
+
+package com.unhappychoice.norimaki.presentation.core
+
+import android.content.Context
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.unhappychoice.norimaki.MainActivity
+import com.unhappychoice.norimaki.presentation.screen.APITokenScreen
+import com.unhappychoice.norimaki.presentation.screen.BuildListScreen
+import com.unhappychoice.norimaki.presentation.screen.core.Screen
+import com.unhappychoice.norimaki.presentation.view.core.BaseView
