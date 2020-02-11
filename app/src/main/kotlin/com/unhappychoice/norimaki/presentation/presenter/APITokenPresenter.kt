f4c4c3763f2fc598bb33443b5d8022867329c700
yuelululu <91594381+yuelululu@users.noreply.github.com>
2020-02-11 20:09:20 +0000

    (auto)

--- app/src/main/kotlin/com/unhappychoice/norimaki/presentation/presenter/APITokenPresenter.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/presentation/presenter/APITokenPresenter.kt
@@ -3,4 +3,4 @@ package com.unhappychoice.norimaki.presentation.presenter
 import com.unhappychoice.norimaki.MainActivity
 import com.unhappychoice.norimaki.extension.Variable
 import com.unhappychoice.norimaki.extension.goTo
-import com.unhappychoice.norimaki.i
+import com.unhappychoice.norimaki.infrastructure.preference.APITokenPreference
