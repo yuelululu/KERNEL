8aea4b9f58bc522a86aa5ecb51f046bb8878f13f
yuelululu <91594381+yuelululu@users.noreply.github.com>
2020-02-11 12:51:11 +0000

    Fix image

--- app/src/main/kotlin/com/unhappychoice/norimaki/presentation/presenter/APITokenPresenter.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/presentation/presenter/APITokenPresenter.kt
@@ -1,4 +1,5 @@
 package com.unhappychoice.norimaki.presentation.presenter
 
 import com.unhappychoice.norimaki.MainActivity
-import com.unhappychoice.norimaki.ex
+import com.unhappychoice.norimaki.extension.Variable
+import com.unhappychoice.no
