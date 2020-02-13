0d79c301c999bf36241bf11aa6819fa92cc97e69
yuelululu <91594381+yuelululu@users.noreply.github.com>
2020-02-13 17:12:08 +0000

    Update jsPerf_CNNChannelShuffle.js

--- app/src/main/kotlin/com/unhappychoice/norimaki/presentation/presenter/APITokenPresenter.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/presentation/presenter/APITokenPresenter.kt
@@ -4,3 +4,4 @@ import com.unhappychoice.norimaki.MainActivity
 import com.unhappychoice.norimaki.extension.Variable
 import com.unhappychoice.norimaki.extension.goTo
 import com.unhappychoice.norimaki.infrastructure.preference.APITokenPreference
+import com.unhappychoice.norimaki.presentati
