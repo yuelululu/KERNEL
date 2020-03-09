97f8296d723b3093c1105d6b97e6f8b67e32e3b7
yuelululu <91594381+yuelululu@users.noreply.github.com>
2020-03-09 18:49:07 +0000

    Fix typo in example

--- app/src/main/kotlin/com/unhappychoice/norimaki/presentation/presenter/BuildListPresenter.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/presentation/presenter/BuildListPresenter.kt
@@ -2,4 +2,5 @@ package com.unhappychoice.norimaki.presentation.presenter
 
 import com.github.unhappychoice.circleci.response.Build
 import com.github.unhappychoice.circleci.response.Project
-import com.gojuno.
+import com.gojuno.koptional.None
+import com.gojuno.koptional.Op
