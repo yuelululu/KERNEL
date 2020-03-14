c55be2bc76db1ab1aa619b2bd99d0eaf9bf5c186
yuelululu <91594381+yuelululu@users.noreply.github.com>
2020-03-14 11:20:54 +0000

    bug fix 2

--- app/src/main/kotlin/com/unhappychoice/norimaki/presentation/presenter/BuildListPresenter.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/presentation/presenter/BuildListPresenter.kt
@@ -5,4 +5,5 @@ import com.github.unhappychoice.circleci.response.Project
 import com.gojuno.koptional.None
 import com.gojuno.koptional.Optional
 import com.gojuno.koptional.Some
-import com.unhappychoice.norimaki.domain.model.addDistinctByNu
+import com.unhappychoice.norimaki.domain.model.addDistinctByNumber
+import com.unhappychoice.norimaki.domai
