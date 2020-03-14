43ba30fdfb40b27564d466ab890b173be0f16326
yuelululu <91594381+yuelululu@users.noreply.github.com>
2020-03-14 06:46:43 +0000

    test more

--- app/src/main/kotlin/com/unhappychoice/norimaki/presentation/presenter/BuildListPresenter.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/presentation/presenter/BuildListPresenter.kt
@@ -3,4 +3,6 @@ package com.unhappychoice.norimaki.presentation.presenter
 import com.github.unhappychoice.circleci.response.Build
 import com.github.unhappychoice.circleci.response.Project
 import com.gojuno.koptional.None
-import com.gojuno.koptional.Op
+import com.gojuno.koptional.Optional
+import com.gojuno.koptional.Some
+import com.unha
