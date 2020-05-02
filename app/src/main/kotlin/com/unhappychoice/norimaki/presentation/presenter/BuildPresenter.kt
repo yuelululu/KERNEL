d9047032fd70f18c28ba7e69fcf2ea4441cfa416
yuelululu <91594381+yuelululu@users.noreply.github.com>
2020-05-02 17:37:05 +0000

    PM: devfreq: Use high priority workqueue

--- app/src/main/kotlin/com/unhappychoice/norimaki/presentation/presenter/BuildPresenter.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/presentation/presenter/BuildPresenter.kt
@@ -1,2 +1,46 @@
 
 package com.unhappychoice.norimaki.presentation.presenter
+
+import com.github.unhappychoice.circleci.response.Build
+import com.github.unhappychoice.circleci.response.BuildStep
+import com.unhappychoice.norimaki.domain.model.addAction
+import com.unhappychoice.norimaki.extension.*
+import com.unhappychoice.norimaki.presentation.presenter.core.PresenterNeedsToken
+import com.unhappychoice.norimaki.presentation.screen.BuildStepScreen
+import com.unhappychoice.norimaki.presentation.view.BuildView
+import io.reactivex.rxkotlin.addTo
+import io.reactivex.rxkotlin.switchLatest
+import mortar.MortarScope
+import org.kodein.di.instance
+
+class BuildPresenter : PresenterNeedsToken<BuildView>() {
+    val build: Build by instance()
+    val steps = Variable<List<BuildStep>>(listOf())
+
+    override fun onEnterScope(scope: MortarScope?) {
+        super.onEnterScope(scope)
+
+        pusher.newActionEvents(build)
+            .map { BuildStep(name = it.log.name, actions = listOf(it.log.toBuildAction())) }
+            .filterNotNull()
+            .subscribeNext { steps.value = steps.value + it }
+            .addTo(bag)
+
+        pusher.updateActionEvents(build)
+            .map { it.log.toBuildAction() }
+            .subscribeNext { steps.value = steps.value.addAction(it) }
+            .addTo(bag)
+
+        getBuild()
+    }
+
+    fun getBuild() {
+        api.getBuild(build.username!!, build.reponame!!, build.buildNum!!)
+            .subscribeOnIoObserveOnUI()
+            .subscribeNext { steps.value = steps.value + (it.steps ?: listOf()) }
+            .addTo(bag)
+    }
+
+    fun goToBuildStepScreen(buildStep: BuildStep) {
+        if (buildStep.actions.isEmpty()) return
+        goTo(activity, BuildStepScreen(build, buildStep))
