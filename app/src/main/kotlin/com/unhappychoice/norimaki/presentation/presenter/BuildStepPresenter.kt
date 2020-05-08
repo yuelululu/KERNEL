f1f38eaa6fd5d9cc233a5ef097f45a44d163f2bc
yuelululu <91594381+yuelululu@users.noreply.github.com>
2020-05-08 19:37:58 +0000

    Updated spec.

--- app/src/main/kotlin/com/unhappychoice/norimaki/presentation/presenter/BuildStepPresenter.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/presentation/presenter/BuildStepPresenter.kt
@@ -1,4 +1,5 @@
 package com.unhappychoice.norimaki.presentation.presenter
 
 import com.github.unhappychoice.circleci.response.Build
-import com.github.unhappychoice.circleci.response.BuildActi
+import com.github.unhappychoice.circleci.response.BuildAction
+import com.github.unhappychoice.
