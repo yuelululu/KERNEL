017b2ef33c2db78d5f74b6bc6446c9afe6cb8d54
yuelululu <91594381+yuelululu@users.noreply.github.com>
2020-05-12 12:58:49 +0000

    temporary fix

--- app/src/main/kotlin/com/unhappychoice/norimaki/presentation/presenter/BuildStepPresenter.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/presentation/presenter/BuildStepPresenter.kt
@@ -2,4 +2,5 @@ package com.unhappychoice.norimaki.presentation.presenter
 
 import com.github.unhappychoice.circleci.response.Build
 import com.github.unhappychoice.circleci.response.BuildAction
-import com.github.unhappychoice.
+import com.github.unhappychoice.circleci.response.BuildStep
+import com.
