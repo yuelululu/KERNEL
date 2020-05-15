4ac5474d676762a0a72bdecad55c2af8e13ac6b3
yuelululu <91594381+yuelululu@users.noreply.github.com>
2020-05-15 19:31:45 +0000

    Fixing lint.

--- app/src/main/kotlin/com/unhappychoice/norimaki/presentation/presenter/BuildStepPresenter.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/presentation/presenter/BuildStepPresenter.kt
@@ -3,4 +3,5 @@ package com.unhappychoice.norimaki.presentation.presenter
 import com.github.unhappychoice.circleci.response.Build
 import com.github.unhappychoice.circleci.response.BuildAction
 import com.github.unhappychoice.circleci.response.BuildStep
-import com.
+import com.unhappychoice.norimaki.domain.model.step
+import com.unhapp
