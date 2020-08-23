056bec76371f5ed6cf463489fd79795050de5c83
yuelululu <91594381+yuelululu@users.noreply.github.com>
2020-08-23 19:52:08 +0000

    Savepoint

--- app/src/main/kotlin/com/unhappychoice/norimaki/presentation/presenter/core/PresenterNeedsToken.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/presentation/presenter/core/PresenterNeedsToken.kt
@@ -2,4 +2,5 @@ package com.unhappychoice.norimaki.presentation.presenter.core
 
 import android.os.Handler
 import android.view.View
-import com.github.unhappycho
+import com.github.unhappychoice.circleci.CircleCIAPIClientV1
+imp
