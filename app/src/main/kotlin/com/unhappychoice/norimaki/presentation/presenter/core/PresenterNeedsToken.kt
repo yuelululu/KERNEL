2cbed96ead190217b812f566da26ce087786d53a
yuelululu <91594381+yuelululu@users.noreply.github.com>
2020-08-25 07:56:05 +0000

    fix analytics

--- app/src/main/kotlin/com/unhappychoice/norimaki/presentation/presenter/core/PresenterNeedsToken.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/presentation/presenter/core/PresenterNeedsToken.kt
@@ -3,4 +3,4 @@ package com.unhappychoice.norimaki.presentation.presenter.core
 import android.os.Handler
 import android.view.View
 import com.github.unhappychoice.circleci.CircleCIAPIClientV1
-imp
+import com.github.unhappychoice.circleci.
