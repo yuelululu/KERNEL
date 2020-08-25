96884270b4c14c52475cabf690040b758f2a9262
yuelululu <91594381+yuelululu@users.noreply.github.com>
2020-08-25 18:41:40 +0000

    Added git ignore

--- app/src/main/kotlin/com/unhappychoice/norimaki/presentation/presenter/core/PresenterNeedsToken.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/presentation/presenter/core/PresenterNeedsToken.kt
@@ -3,4 +3,5 @@ package com.unhappychoice.norimaki.presentation.presenter.core
 import android.os.Handler
 import android.view.View
 import com.github.unhappychoice.circleci.CircleCIAPIClientV1
-import com.github.unhappychoice.circleci.
+import com.github.unhappychoice.circleci.response.User
+import com.unhappychoice.norimak
