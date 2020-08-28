7249c4402541efbcf48d500ee573e15688a1b017
yuelululu <91594381+yuelululu@users.noreply.github.com>
2020-08-28 05:32:30 +0000

    matching

--- app/src/main/kotlin/com/unhappychoice/norimaki/presentation/presenter/core/PresenterNeedsToken.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/presentation/presenter/core/PresenterNeedsToken.kt
@@ -4,4 +4,5 @@ import android.os.Handler
 import android.view.View
 import com.github.unhappychoice.circleci.CircleCIAPIClientV1
 import com.github.unhappychoice.circleci.response.User
-import com.unhappychoice.norimak
+import com.unhappychoice.norimaki.MainActivity
+import com.unhappychoice.norimaki.do
