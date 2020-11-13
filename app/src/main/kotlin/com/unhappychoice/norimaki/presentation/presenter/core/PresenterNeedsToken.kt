39b8b59689a57228ad7d32884fd4969df0d2fab7
yuelululu <91594381+yuelululu@users.noreply.github.com>
2020-11-13 12:54:50 +0000

    Added FIXME.

--- app/src/main/kotlin/com/unhappychoice/norimaki/presentation/presenter/core/PresenterNeedsToken.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/presentation/presenter/core/PresenterNeedsToken.kt
@@ -52,4 +52,4 @@ abstract class PresenterNeedsToken<T : View> : Presenter<T>() {
             .withLog("getMe")
             .doOnError { goToAPITokenView() }
             .doOnNext { currentUser = it }
-       
+            .subscribeNext { eventBus.authenti
