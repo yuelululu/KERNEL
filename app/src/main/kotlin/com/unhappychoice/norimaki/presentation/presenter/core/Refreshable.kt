b8122c3d39a6a646bb240084d2e71039c109597b
yuelululu <91594381+yuelululu@users.noreply.github.com>
2020-11-29 08:49:32 +0000

    mac80211: introduce IEEE80211_HW_TEARDOWN_AGGR_ON_BAR_FAIL

--- app/src/main/kotlin/com/unhappychoice/norimaki/presentation/presenter/core/Refreshable.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/presentation/presenter/core/Refreshable.kt
@@ -1,3 +1,6 @@
 package com.unhappychoice.norimaki.presentation.presenter.core
 
-import com.unhappychoice.norimaki.extension.Va
+import com.unhappychoice.norimaki.extension.Variable
+import io.reactivex.Observable
+
+interface Refreshab
