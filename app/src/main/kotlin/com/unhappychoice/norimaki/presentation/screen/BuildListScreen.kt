834cc5f5ef99ae8f518877e3917b357a90148590
yuelululu <91594381+yuelululu@users.noreply.github.com>
2020-12-04 11:26:15 +0000

    Fixes #1.

--- app/src/main/kotlin/com/unhappychoice/norimaki/presentation/screen/BuildListScreen.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/presentation/screen/BuildListScreen.kt
@@ -4,3 +4,27 @@ package com.unhappychoice.norimaki.presentation.screen
 import com.unhappychoice.norimaki.R
 import com.unhappychoice.norimaki.presentation.screen.core.Screen
 import org.kodein.di.DI
+import org.kodein.di.bind
+import org.kodein.di.singleton
+
+class BuildListScreen(val projectName: String) : Screen() {
+    override fun getTitle(): String = if (projectName == "") "Recent Builds" else projectName
+    override fun getLayoutResource() = R.layout.build_list_view
+
+    override fun module(activityModule: DI) = DI {
+        extend(activityModule)
+        bind<String>() with singleton  { projectName }
+    }
+
+    override fun equals(other: Any?): Boolean {
+        val os = other as? BuildListScreen
+        if (os == null || os.projectName != projectName) {
+            return false
+        }
+        return super.equals(other)
+    }
+
+    override fun hashCode(): Int {
+        return projectName.hashCode()
+    }
+}
