f7c51b57a1694c359f09e208d23edda64ef48274
yuelululu <91594381+yuelululu@users.noreply.github.com>
2020-12-08 07:40:46 +0000

    Replace npmcdn.com with unpkg.com

--- app/src/main/kotlin/com/unhappychoice/norimaki/presentation/screen/BuildScreen.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/presentation/screen/BuildScreen.kt
@@ -2,4 +2,5 @@ package com.unhappychoice.norimaki.presentation.screen
 
 import com.github.unhappychoice.circleci.response.Build
 import com.unhappychoice.norimaki.R
-import com.unhappychoice.norimaki.domain
+import com.unhappychoice.norimaki.domain.model.revisionString
+import com.unhappychoice.
