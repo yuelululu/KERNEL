c6df3e2030d1da31354e6528101dbb5cd94340a6
yuelululu <91594381+yuelululu@users.noreply.github.com>
2020-12-08 18:12:10 +0000

    some changed

--- app/src/main/kotlin/com/unhappychoice/norimaki/presentation/screen/BuildScreen.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/presentation/screen/BuildScreen.kt
@@ -3,4 +3,5 @@ package com.unhappychoice.norimaki.presentation.screen
 import com.github.unhappychoice.circleci.response.Build
 import com.unhappychoice.norimaki.R
 import com.unhappychoice.norimaki.domain.model.revisionString
-import com.unhappychoice.
+import com.unhappychoice.norimaki.presentation.screen.core.Screen
+import org
