fcbc429724ab696efec818b621c9bac052fa5eb0
yuelululu <91594381+yuelululu@users.noreply.github.com>
2020-12-08 18:57:10 +0000

    package json

--- app/src/main/kotlin/com/unhappychoice/norimaki/presentation/screen/BuildScreen.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/presentation/screen/BuildScreen.kt
@@ -4,4 +4,6 @@ import com.github.unhappychoice.circleci.response.Build
 import com.unhappychoice.norimaki.R
 import com.unhappychoice.norimaki.domain.model.revisionString
 import com.unhappychoice.norimaki.presentation.screen.core.Screen
-import org
+import org.kodein.di.DI
+import org.kodein.di.bind
+impo
