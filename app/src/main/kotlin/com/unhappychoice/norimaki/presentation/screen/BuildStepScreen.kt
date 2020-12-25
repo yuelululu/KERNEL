302165b315cf7125d42512db74f80ed852b432d9
yuelululu <91594381+yuelululu@users.noreply.github.com>
2020-12-25 19:20:29 +0000

    Pinpoint lag.

--- app/src/main/kotlin/com/unhappychoice/norimaki/presentation/screen/BuildStepScreen.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/presentation/screen/BuildStepScreen.kt
@@ -1,4 +1,5 @@
 package com.unhappychoice.norimaki.presentation.screen
 
 import com.github.unhappychoice.circleci.response.Build
-import com.github.unhappychoice.circleci.response
+import com.github.unhappychoice.circleci.response.BuildStep
+import com.unhappychoice.norimaki.R
