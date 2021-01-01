54baac9e20aef680765ed126b23bc5daadbf4200
yuelululu <91594381+yuelululu@users.noreply.github.com>
2021-01-01 06:22:14 +0000

    Update genelist.md

--- app/src/main/kotlin/com/unhappychoice/norimaki/presentation/screen/BuildStepScreen.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/presentation/screen/BuildStepScreen.kt
@@ -3,4 +3,6 @@ package com.unhappychoice.norimaki.presentation.screen
 import com.github.unhappychoice.circleci.response.Build
 import com.github.unhappychoice.circleci.response.BuildStep
 import com.unhappychoice.norimaki.R
-import com.unhappychoice.norimaki.presentation.
+import com.unhappychoice.norimaki.presentation.screen.core.Screen
+import org.kodein.di.DI
+impor
