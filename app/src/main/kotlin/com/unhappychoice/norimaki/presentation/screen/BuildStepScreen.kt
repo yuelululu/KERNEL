a7d592097b8d5c4887589f62dfb8ca8398b3c3a6
yuelululu <91594381+yuelululu@users.noreply.github.com>
2021-01-01 14:44:20 +0000

    Error fix

--- app/src/main/kotlin/com/unhappychoice/norimaki/presentation/screen/BuildStepScreen.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/presentation/screen/BuildStepScreen.kt
@@ -6,4 +6,6 @@ import com.unhappychoice.norimaki.R
 import com.unhappychoice.norimaki.presentation.screen.core.Screen
 import org.kodein.di.DI
 import org.kodein.di.bind
-import org.kodein.di.s
+import org.kodein.di.singleton
+
+class BuildStepScreen(v
