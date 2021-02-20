03839fdd4cb4b4e94b6188da19d4efbcc467a327
yuelululu <91594381+yuelululu@users.noreply.github.com>
2021-02-20 09:52:26 +0000

    Fix PEP8 errors

--- app/src/main/kotlin/com/unhappychoice/norimaki/presentation/view/APITokenView.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/presentation/view/APITokenView.kt
@@ -3,4 +3,5 @@ package com.unhappychoice.norimaki.presentation.view
 import android.content.Context
 import android.util.AttributeSet
 import android.util.Log
-import android.vi
+import android.view.LayoutInflater
+import com.jakewharton.rxbindi
