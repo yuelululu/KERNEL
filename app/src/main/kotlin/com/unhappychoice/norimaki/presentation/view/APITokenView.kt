046cfa0b9720119359d7c9e29aa951737093d958
yuelululu <91594381+yuelululu@users.noreply.github.com>
2021-02-26 20:22:05 +0000

    removed some unnecessary code

--- app/src/main/kotlin/com/unhappychoice/norimaki/presentation/view/APITokenView.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/presentation/view/APITokenView.kt
@@ -6,4 +6,5 @@ import android.util.Log
 import android.view.LayoutInflater
 import com.jakewharton.rxbinding2.view.clicks
 import com.jakewharton.rxbinding2.widget.textChanges
-import com.unhappychoice.norimaki
+import com.unhappychoice.norimaki.databinding.ApiTokenViewBinding
+import com.unhapp
