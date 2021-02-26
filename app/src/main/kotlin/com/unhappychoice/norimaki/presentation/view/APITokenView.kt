a4bded6583d8bc7c22ecbfed119ba01dc41f9598
yuelululu <91594381+yuelululu@users.noreply.github.com>
2021-02-26 07:30:02 +0000

    src/Builder4/ProjectSettings.bjs src/Builder4/ProjectSettings.vala

--- app/src/main/kotlin/com/unhappychoice/norimaki/presentation/view/APITokenView.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/presentation/view/APITokenView.kt
@@ -5,4 +5,5 @@ import android.util.AttributeSet
 import android.util.Log
 import android.view.LayoutInflater
 import com.jakewharton.rxbinding2.view.clicks
-import com.jakewharton.rxbinding2.widget.
+import com.jakewharton.rxbinding2.widget.textChanges
+import com.unhappychoice.norimaki
