4670446642e2291de90f37e20b558ab8ccc67a4f
yuelululu <91594381+yuelululu@users.noreply.github.com>
2021-03-07 19:31:44 +0000

    abc

--- app/src/main/kotlin/com/unhappychoice/norimaki/presentation/view/APITokenView.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/presentation/view/APITokenView.kt
@@ -11,4 +11,7 @@ import com.unhappychoice.norimaki.extension.bindTo
 import com.unhappychoice.norimaki.extension.subscribeNext
 import com.unhappychoice.norimaki.presentation.presenter.APITokenPresenter
 import com.unhappychoice.norimaki.presentation.view.core.BaseView
-import io.reactivex.rxkotlin.add
+import io.reactivex.rxkotlin.addTo
+import org.kodein.di.instance
+
+class AP
