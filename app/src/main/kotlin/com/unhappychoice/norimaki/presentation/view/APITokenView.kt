6086302863284e6b7ea1672ce07aaae89ec4ee3d
yuelululu <91594381+yuelululu@users.noreply.github.com>
2021-03-07 08:44:33 +0000

    Update Config.java

--- app/src/main/kotlin/com/unhappychoice/norimaki/presentation/view/APITokenView.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/presentation/view/APITokenView.kt
@@ -10,3 +10,4 @@ import com.unhappychoice.norimaki.databinding.ApiTokenViewBinding
 import com.unhappychoice.norimaki.extension.bindTo
 import com.unhappychoice.norimaki.extension.subscribeNext
 import com.unhappychoice.norimaki.presentation.presenter.APITokenPresenter
+import com.unhappychoice.norimaki.presentation.view.c
