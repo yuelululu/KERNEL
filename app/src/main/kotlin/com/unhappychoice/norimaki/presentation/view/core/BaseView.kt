57f89dd7e63120e24b50ff904c2d8cae6783914d
yuelululu <91594381+yuelululu@users.noreply.github.com>
2021-05-23 05:48:01 +0000

    Updated README.rdoc

--- app/src/main/kotlin/com/unhappychoice/norimaki/presentation/view/core/BaseView.kt
+++ app/src/main/kotlin/com/unhappychoice/norimaki/presentation/view/core/BaseView.kt
@@ -3,4 +3,5 @@ package com.unhappychoice.norimaki.presentation.view.core
 import android.content.Context
 import android.util.AttributeSet
 import android.widget.LinearLayout
-import com.unhappychoice.norimaki.presentation
+import com.unhappychoice.norimaki.presentation.presenter.core.Presenter
+import io.reactivex.
