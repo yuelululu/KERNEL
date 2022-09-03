cbb4f2e2103df5f44c891935d6ad0ee4891fe8ff
yuelululu <91594381+yuelululu@users.noreply.github.com>
2022-09-03 09:44:08 +0000

    0.6.0 release

--- app/src/test/kotlin/com/unhappychoice/norimaki/presentation/screen/BuildListScreenTest.kt
+++ app/src/test/kotlin/com/unhappychoice/norimaki/presentation/screen/BuildListScreenTest.kt
@@ -3,4 +3,6 @@ package com.unhappychoice.norimaki.presentation.screen
 import com.unhappychoice.norimaki.R
 import com.winterbe.expekt.expect
 import io.polymorphicpanda.kspec.KSpec
-import io.polymorphicpanda.kspec.descr
+import io.polymorphicpanda.kspec.describe
+import io.polymorphicpanda.kspec.it
+import io.p
