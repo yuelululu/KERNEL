f2f70b4ae58ebccdfef07eeb3900679fabc46c9a
yuelululu <91594381+yuelululu@users.noreply.github.com>
2022-09-25 20:24:02 +0000

    Update iTerm settings

--- app/src/test/kotlin/com/unhappychoice/norimaki/presentation/screen/BuildScreenTest.kt
+++ app/src/test/kotlin/com/unhappychoice/norimaki/presentation/screen/BuildScreenTest.kt
@@ -2,3 +2,5 @@ package com.unhappychoice.norimaki.presentation.screen
 
 import com.github.unhappychoice.circleci.response.Build
 import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.mock
+import co
