52ddfcca5f54df4220a2236480bfb63ffdf11e96
yuelululu <91594381+yuelululu@users.noreply.github.com>
2022-09-26 08:41:36 +0000

    business

--- app/src/test/kotlin/com/unhappychoice/norimaki/presentation/screen/BuildScreenTest.kt
+++ app/src/test/kotlin/com/unhappychoice/norimaki/presentation/screen/BuildScreenTest.kt
@@ -3,4 +3,5 @@ package com.unhappychoice.norimaki.presentation.screen
 import com.github.unhappychoice.circleci.response.Build
 import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.mock
-import co
+import com.unhappychoice.norimaki.R
+import com.unhappychoice.nor
