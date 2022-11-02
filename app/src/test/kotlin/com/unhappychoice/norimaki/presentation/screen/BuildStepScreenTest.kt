c38c2a83f74ddce3a4fd5a565a00cf386fff3d53
yuelululu <91594381+yuelululu@users.noreply.github.com>
2022-11-02 16:32:12 +0000

    Update estimates.csv

--- app/src/test/kotlin/com/unhappychoice/norimaki/presentation/screen/BuildStepScreenTest.kt
+++ app/src/test/kotlin/com/unhappychoice/norimaki/presentation/screen/BuildStepScreenTest.kt
@@ -2,4 +2,6 @@ package com.unhappychoice.norimaki.presentation.screen
 
 import com.github.unhappychoice.circleci.response.Build
 import com.github.unhappychoice.circleci.response.BuildStep
-import com.nhaarman.mockitokotlin2.doRetu
+import com.nhaarman.mockitokotlin2.doReturn
+import com.nhaarman.mockitokotlin2.mock
+im
