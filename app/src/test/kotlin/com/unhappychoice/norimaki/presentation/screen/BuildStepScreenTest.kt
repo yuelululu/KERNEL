dff1e9b93b0ec40b5d4ab1ce88899b58ff08bb94
yuelululu <91594381+yuelululu@users.noreply.github.com>
2022-11-05 09:36:25 +0000

    Fix mapping

--- app/src/test/kotlin/com/unhappychoice/norimaki/presentation/screen/BuildStepScreenTest.kt
+++ app/src/test/kotlin/com/unhappychoice/norimaki/presentation/screen/BuildStepScreenTest.kt
@@ -4,4 +4,5 @@ import com.github.unhappychoice.circleci.response.Build
 import com.github.unhappychoice.circleci.response.BuildStep
 import com.nhaarman.mockitokotlin2.doReturn
 import com.nhaarman.mockitokotlin2.mock
-im
+import com.unhappychoice.norimaki.R
+import
