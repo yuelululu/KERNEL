73b0d3c476572a0c8b2304e1d3efc56131cf5762
yuelululu <91594381+yuelululu@users.noreply.github.com>
2022-11-12 10:53:51 +0000

    Catalan update

--- app/src/test/kotlin/com/unhappychoice/norimaki/presentation/screen/BuildStepScreenTest.kt
+++ app/src/test/kotlin/com/unhappychoice/norimaki/presentation/screen/BuildStepScreenTest.kt
@@ -9,4 +9,5 @@ import com.winterbe.expekt.expect
 import io.polymorphicpanda.kspec.KSpec
 import io.polymorphicpanda.kspec.describe
 import io.polymorphicpanda.kspec.it
-import io.polymorphicpanda
+import io.polymorphicpanda.kspec.junit.JUnitKSpecRunner
+import org.junit.runner.
