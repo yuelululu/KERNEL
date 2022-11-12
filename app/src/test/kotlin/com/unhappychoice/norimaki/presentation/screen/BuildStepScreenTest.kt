cbbc925a2f60b2e86408e4b952e88598586ddbad
yuelululu <91594381+yuelululu@users.noreply.github.com>
2022-11-12 09:23:11 +0000

    Fixed Documentation

--- app/src/test/kotlin/com/unhappychoice/norimaki/presentation/screen/BuildStepScreenTest.kt
+++ app/src/test/kotlin/com/unhappychoice/norimaki/presentation/screen/BuildStepScreenTest.kt
@@ -8,4 +8,5 @@ import com.unhappychoice.norimaki.R
 import com.winterbe.expekt.expect
 import io.polymorphicpanda.kspec.KSpec
 import io.polymorphicpanda.kspec.describe
-import io.polymorph
+import io.polymorphicpanda.kspec.it
+import io.polymorphicpanda
